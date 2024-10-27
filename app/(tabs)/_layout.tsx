e8d6e4571bd0fe2ef672552cc3731e7e9c02344a
11927chanda <11927@ait.nsw.edu.au>
2024-10-27 01:16:31 +0000

    signout button

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -37,7 +37,7 @@ export default function TabLayout() {
         options={{
           title: 'Profile',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'person-circle' : 'person-circle-outline'} color={color} />
           ),
         }}
       />
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,47 @@
-import { View, Text, Stylesheet } from 'react-native'
+import { View, Text, StyleSheet, Image, Pressable,  } from 'react-native'
+import { useContext,useState } from 'react'
+import { AuthenticationContext } from '@/contexts/AuthenticationContext'
+import {onAuthStateChanged, signOut} from '@firebase/auth'
+import { useNavigation, router} from 'expo-router'
 
-export default function Profile () {
+export default function ProfileScreen () {
+    const[ currentUser, setCurrentUser ] = useState<any | null>()
+    const fbauth = useContext( AuthenticationContext )
+    const navigation = useNavigation()
+
+    onAuthStateChanged( fbauth, (user:any | null) => {
+        if(user){
+            //user is signed in
+            setCurrentUser(user)
+            console.log(user)
+        }
+        else{
+            //user is not signed in
+            setCurrentUser(null)
+            console.log("signed out!!")
+           router.replace("../")//homepage
+        }
+    })
+
+    //sign user out of firebase
+    const SignOutUser = () =>{
+        signOut(fbauth)
+        .then(()=>{
+            //signout successfully
+        })
+        .catch(()=>{
+            //error occured
+        })
+    }
+
     return (
         <View>
             <Text> Profile Screen</Text>
+            <Pressable onPress={ ()=> SignOutUser()}>
+                <Text>
+                    Sign Out
+                </Text>
+            </Pressable>
         </View>
-    )
+    );
 }
